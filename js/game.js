8de443e404cd02229cf67b31c1ddbbdc09e1fa42
chieh0908 <xxaaxacc@gmail.com>
2023-05-19 22:31:33 +0800

    food rwd

--- js/game.js
+++ js/game.js
@@ -13,22 +13,39 @@ function startAnimation() {
     document.getElementById(`result`).style.transform = `translateY(${-num * 10}%)`;
     document.getElementsByClassName('cont_principal')[num].style.display = 'block';
     document.getElementsByClassName('coupon')[num].style.display = 'block';
+    
     if (prevCartTop) {
         prevCartTop.style.display = 'none';
       }
       prevCartTop = document.getElementsByClassName('cont_principal')[num];
       prevCartTop.style.display = 'block';
   }
-
+  
 }
 
+
 window.onload = function () {
-    document.querySelector('.cont_modal').className = "cont_modal";
-  }
+  document.querySelector('.cont_modal').className = "cont_modal";
+}
   
-  let c = 0;
-  function open_close() {
-    document.querySelector('.cont_modal').className =
-      c % 2 == 0 ? "cont_modal cont_modal_active" : "cont_modal";
-    c++;
-  }
+let c = 0;
+function open_close() {
+  document.querySelector('.cont_modal').className =
+    c % 2 == 0 ? "cont_modal cont_modal_active" : "cont_modal";
+  c++;
+}
+
+const select = (DOM) => document.querySelector(DOM);
+
+select('#coupon_btn').addEventListener('click', (e) => {
+  // 建立 Range 物件
+  const range = document.createRange();
+  const texts = select('#text');
+  range.selectNode(texts);
+  // 取得 Selection 物件
+  const selection = window.getSelection();
+  selection.removeAllRanges();
+  selection.addRange(range);
+  document.execCommand("copy");
+  selection.removeAllRanges();
+})
