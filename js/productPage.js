7fdf33b237f6ff2ab3d164733de699f46b39a954
WEI19981006 <leejawei987@gmail.com>
2023-05-27 23:46:43 +0800

    productPage資料庫串接 圖片上傳

--- js/productPage.js
+++ js/productPage.js
@@ -25,32 +25,125 @@ const app = Vue.createApp({
             Introduction1: '',
             Introduction2: '',
             Introduction3: '',
+
+            //方案詳情
+            detail1_Time: '',
+            detail1_ScheduleTitle: '',
+            detail1_Pic: '',
+            detail1_Content: '',
+
+            detail2_Time: '',
+            detail2_ScheduleTitle: '',
+            detail2_Pic: '',
+            detail2_Content: '',
+            detail2_ContentTitle: '',
+
+            detail3_Time: '',
+            detail3_ScheduleTitle: '',
+            detail3_Pic: '',
+            detail3_Content: '',
+            detail3_ContentTitle: '',
+
+            detail4_Time: '',
+            detail4_ScheduleTitle: '',
+            detail4_Pic: '',
+            detail4_Content: '',
+            detail4_ContentTitle: '',
+
+            detail5_Time: '',
+            detail5_ScheduleTitle: '',
         }
     },
     created() {
 
+
+        //傳遞id的值到php
+
+        // 這段程式碼定義了一個名為 getParameterByName 的函式，用於從 URL 中獲取指定名稱的參數值。
+
+        // 程式碼中的 getParameterByName 函式使用正則表達式來解析 URL，並通過指定的參數名稱返回相應的參數值。
+        
+        // 使用方法如下：
+        
+        // 呼叫 getParameterByName 函式並傳入參數名稱，例如 'id'。
+        // 如果沒有指定第二個參數 url，則預設使用 window.location.href 獲取當前頁面的 URL。
+        // getParameterByName 函式會使用正則表達式解析 URL，找到名稱為參數名稱的參數並返回其值。
+        // 如果找不到指定名稱的參數，則返回 null。
+        // 如果找到指定名稱的參數但值為空，則返回空字串 ""。
+        // 如果找到指定名稱的參數並存在值，則返回解碼後的參數值。
+        // 在提供的程式碼中，我們使用 getParameterByName 函式來獲取 URL 中的 id 參數值，並將其存儲在 productId 變數中。
+        // 然後你可以根據需要使用 productId 變數進行後續處理和使用，例如將其印出到控制台。
+        function getParameterByName(name, url) {
+            if (!url) url = window.location.href;
+            name = name.replace(/[\[\]]/g, "\\$&");
+            var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+                results = regex.exec(url);
+            if (!results) return null;
+            if (!results[2]) return "";
+            return decodeURIComponent(results[2].replace(/\+/g, " "));
+        }
+
+        // 使用 getParameterByName 函式來獲取 URL 中的 id 參數值
+        var productId = getParameterByName('id');
+
+        // 現在你可以使用 productId 變數來進一步處理和使用該值
+        // 例如：
+        console.log(productId); // 印出 id 參數的值
+
+
+
         //撈取資料 丟到data
-        axios.get('../php/productPage.php')
+        axios.get('../php/productPage.php?id=' + productId)
             .then((response) => {
                 //被包裝成二維陣列
-                console.log(response.data[0]['Price']);
-                this.title = response.data[0]['Name'];
-                this.banner1 = `./image/productPage/${response.data[0]['ProductID']}/${response.data[0]['Banner1']}`;
-                this.banner2 = `./image/productPage/${response.data[0]['ProductID']}/${response.data[0]['Banner2']}`;
-                this.banner3 = `./image/productPage/${response.data[0]['ProductID']}/${response.data[0]['Banner3']}`;
-                this.phone_banner1 = `./image/productPage/${response.data[0]['ProductID']}/${response.data[0]['Banner1_m']}`;
-                this.phone_banner2 = `./image/productPage/${response.data[0]['ProductID']}/${response.data[0]['Banner2_m']}`;
-                this.phone_banner3 = `./image/productPage/${response.data[0]['ProductID']}/${response.data[0]['Banner3_m']}`;
-                this.eventContent = response.data[0]['Content'];
-                this.event_pic1 = `./image/productPage/${response.data[0]['ProductID']}/${response.data[0]['Image1']}`;
-                this.event_pic2 = `./image/productPage/${response.data[0]['ProductID']}/${response.data[0]['Image2']}`;
-                this.event_pic3 = `./image/productPage/${response.data[0]['ProductID']}/${response.data[0]['Image3']}`;
-                this.event_pic4 = `./image/productPage/${response.data[0]['ProductID']}/${response.data[0]['Image4']}`;
-                this.Introduction1 = response.data[0]['Content1'];
-                this.Introduction2 = response.data[0]['Content2'];
-                this.Introduction3 = response.data[0]['Content3'];
-                this.total = response.data[0]['Price'];
-                this.price = response.data[0]['Price'];
+                console.log(response.data.data1);
+
+                this.title = response.data.data1[0]['Name'];
+                this.banner1 = `./image/productPage/${response.data.data1[0]['ProductID']}/${response.data.data1[0]['Banner1']}`;
+                this.banner2 = `./image/productPage/${response.data.data1[0]['ProductID']}/${response.data.data1[0]['Banner2']}`;
+                this.banner3 = `./image/productPage/${response.data.data1[0]['ProductID']}/${response.data.data1[0]['Banner3']}`;
+                this.phone_banner1 = `./image/productPage/${response.data.data1[0]['ProductID']}/${response.data.data1[0]['Banner1_m']}`;
+                this.phone_banner2 = `./image/productPage/${response.data.data1[0]['ProductID']}/${response.data.data1[0]['Banner2_m']}`;
+                this.phone_banner3 = `./image/productPage/${response.data.data1[0]['ProductID']}/${response.data.data1[0]['Banner3_m']}`;
+                this.eventContent = response.data.data1[0]['Content'];
+                this.event_pic1 = `./image/productPage/${response.data.data1[0]['ProductID']}/${response.data.data1[0]['Image1']}`;
+                this.event_pic2 = `./image/productPage/${response.data.data1[0]['ProductID']}/${response.data.data1[0]['Image2']}`;
+                this.event_pic3 = `./image/productPage/${response.data.data1[0]['ProductID']}/${response.data.data1[0]['Image3']}`;
+                this.event_pic4 = `./image/productPage/${response.data.data1[0]['ProductID']}/${response.data.data1[0]['Image4']}`;
+                this.Introduction1 = response.data.data1[0]['Content1'];
+                this.Introduction2 = response.data.data1[0]['Content2'];
+                this.Introduction3 = response.data.data1[0]['Content3'];
+                this.total = response.data.data1[0]['Price'];
+                this.price = response.data.data1[0]['Price'];
+
+
+                //data2 是方案詳情
+                console.log(response.data.data2);
+                this.detail1_Time = response.data.data2[0]['Times'];
+                this.detail1_ScheduleTitle = response.data.data2[0]['ScheduleTitle'];
+                this.detail1_Pic = `./image/productPage/${response.data.data1[0]['ProductID']}/${response.data.data2[0]['Image']}`;
+                this.detail1_Content = response.data.data2[0]['Content'];
+
+                this.detail2_Time = response.data.data2[1]['Times'];
+                this.detail2_ScheduleTitle = response.data.data2[1]['ScheduleTitle'];
+                this.detail2_Pic = `./image/productPage/${response.data.data1[0]['ProductID']}/${response.data.data2[1]['Image']}`;
+                this.detail2_Content = response.data.data2[1]['Content'];
+                this.detail2_ContentTitle = response.data.data2[1]['ContentTitle'];
+
+                this.detail3_Time = response.data.data2[2]['Times'];
+                this.detail3_ScheduleTitle = response.data.data2[2]['ScheduleTitle'];
+                this.detail3_Pic = `./image/productPage/${response.data.data1[0]['ProductID']}/${response.data.data2[2]['Image']}`;
+                this.detail3_Content = response.data.data2[2]['Content'];
+                this.detail3_ContentTitle = response.data.data2[2]['ContentTitle'];
+
+                this.detail4_Time = response.data.data2[3]['Times'];
+                this.detail4_ScheduleTitle = response.data.data2[3]['ScheduleTitle'];
+                this.detail4_Pic = `./image/productPage/${response.data.data1[0]['ProductID']}/${response.data.data2[3]['Image']}`;
+                this.detail4_Content = response.data.data2[3]['Content'];
+                this.detail4_ContentTitle = response.data.data2[3]['ContentTitle'];
+
+                this.detail5_Time = response.data.data2[4]['Times'];
+                this.detail5_ScheduleTitle = response.data.data2[4]['ScheduleTitle'];
             })
             .catch((error) => {
                 console.log(error);
--- php/productPage.php
+++ php/productPage.php
@@ -2,21 +2,46 @@
 
 include('Mysql.php'); //資料庫連線
 
-$sql = "SELECT *
-FROM product
-JOIN productIntroduction ON product.IntroductionID = productIntroduction.ID
-JOIN region ON product.RegionID = region.ID 
-WHERE ProductID = 2";
+// 從 URL 的 id 參數中獲取 productId 值
+$productId = $_GET['id'];
 
+// 第一個查詢
+$sql = "SELECT *
+    FROM product
+    JOIN productIntroduction ON product.IntroductionID = productIntroduction.ID
+    JOIN region ON product.RegionID = region.ID 
+    WHERE ProductID = :productId";
 
 $statement = $pdo->prepare($sql);
+$statement->bindParam(':productId', $productId, PDO::PARAM_INT);
 $statement->execute();
 $data = $statement->fetchAll();
-// 将数据转换为 JSON
-$jsonData = json_encode($data);
 
-// 设置响应头，指定内容类型为 JSON
+// 第二個查詢
+$sql2 = "SELECT productSchedule.Times, productSchedule.ScheduleTitle, productSchedule.Content,productSchedule.Image,productSchedule.ContentTitle
+    FROM product
+    JOIN productIntroduction ON product.IntroductionID = productIntroduction.ID
+    JOIN region ON product.RegionID = region.ID
+    JOIN productSchedule ON product.ProductID = productSchedule.productID
+    WHERE product.ProductID = :productId";
+
+$statement2 = $pdo->prepare($sql2);
+$statement2->bindParam(':productId', $productId, PDO::PARAM_INT);
+$statement2->execute();
+$data2 = $statement2->fetchAll();
+
+// 將兩個查詢組成陣列
+$response = [
+    'data1' => $data,
+    'data2' => $data2
+];
+
+// 轉 JSON
+$jsonData = json_encode($response);
+
+// 指定內容類型為 JSON
 header('Content-Type: application/json');
 
 echo $jsonData;
+
 ?>
