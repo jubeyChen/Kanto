31a8a0849df6650b705500cf7740850a7d8af510
jubeyChen <jubeyletgo@gmail.com>
2023-05-31 15:01:58 +0800

    會員中心-訂單管理，完成會員評論功能&修改聯絡客服bug

--- php/GetReview.php
+++ php/GetReview.php
@@ -8,20 +8,15 @@ header('Content-Type: application/json');
 // 解析 JSON 資料
 $data = json_decode(file_get_contents('php://input'), true);
 
-$id = htmlspecialchars($data['user']);
+$MemberID = htmlspecialchars($data['MemberID']);
 
-$sql = "SELECT od.ID as OrderNumber, pd.OfferDate, p.ID as ProductID, p.Name, p.Banner1, p.Price,od.IsCanceled, od.Reviewed FROM orders o
-	join members m on o.MemberID = m.ID
-    join orderDetail od on o.ID = od.OrderID
-    join product p on p.ID = od.ProductID
-    join productDetail pd on pd.ID = od.productDetailID
-    where m.AccountID = ? and od.IsCanceled =0";
+$sql = "SELECT * FROM review where MemberID = ?";
 
 //執行並查詢，會回傳查詢結果的物件，必須使用fetch、fetchAll...等方式取得資料
 
 
 $statement = $pdo->prepare($sql);
-$statement->bindParam(1, $id); //檢查 第一個問號
+$statement->bindParam(1, $MemberID); 
 $statement->execute();
 
 
@@ -32,7 +27,8 @@ $data = $statement->fetchAll();
 if(count($data) > 0){
 echo json_encode($data);
 } else{
-   echo 'no order';
+    $data=[];
+    echo json_encode($data);
    }
        
 ?>
---
+++ php/SaveReview.php
@@ -0,0 +1,102 @@
+<?php
+
+include('Mysql.php'); //資料庫連線
+
+header('Content-Type: application/json');
+
+$ProductID = htmlspecialchars($_POST['ProductID']);
+$MemberID = htmlspecialchars($_POST['MemberID']);
+$Star = htmlspecialchars($_POST['Star']);
+$Text = htmlspecialchars($_POST['Text']);
+$OrderDetailID = htmlspecialchars($_POST['OrderDetailID']);
+
+
+//Web根目錄真實路徑, ex: C:/XAMPP/htdocs
+$ServerRoot = $_SERVER["DOCUMENT_ROOT"];
+
+if(isset($_FILES["IMG"])){
+
+//取得上傳的檔案資訊(陣列型態)=============================
+       $fileName_arr = $_FILES["IMG"]["name"];    //檔案名稱含副檔名    
+       $fileTmpName_arr = $_FILES["IMG"]["tmp_name"]; //Server上的暫存檔路徑含檔名    
+       $fileType_arr = $_FILES["IMG"]["type"];    //檔案種類        
+       $fileSize_arr = $_FILES["IMG"]["size"];    //檔案尺寸
+       $error_arr = $_FILES["IMG"]["error"];  //錯誤代碼
+    //=======================================================
+    
+
+    //依上傳檔案的數量跑迴圈一一處理
+       for ($i = 0; $i < count($fileName_arr); $i++) {        
+
+        //Server上的暫存檔路徑含檔名
+       $filePath_Temp = $fileTmpName_arr[$i];
+        
+      $path = $ServerRoot."/Kanto/image/productPage/".$ProductID."/reviewPhoto/";
+      if (!is_dir($path)) {
+       mkdir($path, 0777, true);
+     }
+       $filePath = $path.$fileName_arr[$i];
+
+       $newFileName = time().$i.".".getExtensionName($filePath);
+
+       $newFilePath = $path.$newFileName;
+
+       $fileNames[] = $newFileName;
+
+       //判斷是否上傳成功
+       if($error_arr[$i] > 0){
+            echo "上傳失敗: 錯誤代碼".$error_arr[$i];
+        }else{
+            //將暫存檔搬移到正確位置
+            move_uploaded_file($filePath_Temp, $newFilePath);
+        }
+       }  
+}
+    
+
+    //取得檔案副檔名
+    function getExtensionName($filePath){
+        $path_parts = pathinfo($filePath);
+        return $path_parts["extension"];
+    }
+
+
+
+$sql = "INSERT INTO review(`Text`, MemberID, Star, ProductID, OrderDetailID, Image1, Image2, Image3, Image4, Image5, Image6) 
+VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+
+$statement = $pdo->prepare($sql);
+$statement->bindParam(1, $Text);
+$statement->bindParam(2, $MemberID);
+$statement->bindParam(3, $Star);
+$statement->bindParam(4, $ProductID);
+$statement->bindParam(5, $OrderDetailID);
+
+// 绑定每个文件名到相应的列中，如果没有文件名则绑定为NULL
+for ($j = 0; $j < 6; $j++) {
+    if (isset($fileNames[$j])) {
+        $statement->bindParam($j + 6, $fileNames[$j]);
+    } else {
+        $statement->bindValue($j + 6, NULL, PDO::PARAM_NULL);
+    }
+}
+
+$affectedRow = $statement->execute();
+
+if($affectedRow > 0){
+
+    $sql2 = "UPDATE mydb.orderDetail SET Reviewed = 1 WHERE ID = ?";
+    $statement2 = $pdo->prepare($sql2);
+    $statement2->bindParam(1, $OrderDetailID);
+    $affectedRow2 = $statement2->execute();
+
+    if($affectedRow2 > 0){
+        echo "done";
+    }
+
+       }else{
+              echo "fail";
+       }
+       
+?>
