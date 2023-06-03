5641efd4b202ab3350b99409eb7a6b88b2a2ad35
LOUISJR0304 <louisbearss@gmail.com>
2023-06-03 17:55:08 +0800

    product 篩選功能完成~

--- js/product.js
+++ js/product.js
@@ -17,52 +17,47 @@ for (let i = 0; i < labels.length; i++) {
     });
 }
 
+// 設置當前日期
+let currentDate = new Date();
 
+// 顯示日曆
+function showCalendar(year, month) {
+    const currentMonth = document.querySelector('.current-month');
+    const calendarDays = document.querySelector('.calendar-days');
+    // 設置月份標題
+    const months = ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'];
+    currentMonth.innerHTML = `${year} 年 ${months[month]}`;
+
+    // 設置日期
+    let firstDay = new Date(year, month, 1).getDay(); // 獲取當月第一天是星期幾
+    let lastDate = new Date(year, month + 1, 0).getDate(); // 獲取當月最後一天的日期
+
+    let daysHTML = '';
+
+    // 添加前面的空白
+    for (let i = 0; i < firstDay; i++) {
+        daysHTML += '<div class="calendar-day"></div>';
+    }
+
+    // 添加日期
+    for (let i = 1; i <= lastDate; i++) {
+        // 判斷當前日期是否為當前選中日期，如果是，則添加 .selected 類名
+        let className = '';
+        if (currentDate.getFullYear() === year && currentDate.getMonth() === month && currentDate.getDate() === i) {
+            className = 'selected';
+        }
+        daysHTML += `<div class="calendar-day ${className}">${i}</div>`;
+    }
+    calendarDays.innerHTML = daysHTML;
+}
 
 $(document).ready(function () {
 
-
-    // 操控日期
-
     // 獲取元素
-    const currentMonth = document.querySelector('.current-month');
     const prevMonth = document.querySelector('.prev-month');
     const nextMonth = document.querySelector('.next-month');
-    const calendarDays = document.querySelector('.calendar-days');
 
-    // 設置當前日期
-    let currentDate = new Date();
-
-    // 顯示日曆
-    function showCalendar(year, month) {
-        // 設置月份標題
-        const months = ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'];
-        currentMonth.innerHTML = `${year} 年 ${months[month]}`;
-
-        // 設置日期
-        let firstDay = new Date(year, month, 1).getDay(); // 獲取當月第一天是星期幾
-        let lastDate = new Date(year, month + 1, 0).getDate(); // 獲取當月最後一天的日期
-
-        let daysHTML = '';
-
-        // 添加前面的空白
-        for (let i = 0; i < firstDay; i++) {
-            daysHTML += '<div class="calendar-day"></div>';
-        }
-
-        // 添加日期
-        for (let i = 1; i <= lastDate; i++) {
-            // 判斷當前日期是否為當前選中日期，如果是，則添加 .selected 類名
-            let className = '';
-            if (currentDate.getFullYear() === year && currentDate.getMonth() === month && currentDate.getDate() === i) {
-                className = 'selected';
-            }
-            daysHTML += `<div class="calendar-day ${className}">${i}</div>`;
-        }
-
-        calendarDays.innerHTML = daysHTML;
-    }
-
+    // 操控日期
     // 初始化日曆
     showCalendar(currentDate.getFullYear(), currentDate.getMonth());
 
@@ -79,19 +74,28 @@ $(document).ready(function () {
     });
 
     // 選擇日期
-    calendarDays.addEventListener('click', function (event) {
-        // 判斷點擊的是否為日期
-        if (event.target.classList.contains('calendar-day')) {
-            // 獲取選中的日期
-            let selectedDate = event.target.innerText;
-            // 設置選中的日期
-            currentDate.setDate(selectedDate);
-            // 重新顯示日曆
-            showCalendar(currentDate.getFullYear(), currentDate.getMonth());
-            // 更新Vue中的selectedDate資料
-            this.selectedDate = currentDate.toISOString().substring(0, 10);
-        }
-    }.bind(this)); // 使用bind(this)將事件處理函式綁定到Vue實例
+    // calendarDays.addEventListener('click', (event) => {
+    //     // 判斷點擊的是否為日期
+    //     if (event.target.classList.contains('calendar-day')) {
+    //         // datep(event, currentDate);
+    //         // 獲取選中的日期
+    //         let selectedDate = event.target.innerText;
+    //         // 將選中的日期轉換為符合 OfferDate 格式的日期字串
+    //         const year = currentDate.getFullYear();
+    //         const month = currentDate.getMonth() + 1; // 月份從0開始，所以要加1
+    //         const formattedDate = `${year}-${month.toString().padStart(2, '0')}-${selectedDate.toString().padStart(2, '0')}`;
+
+    //         console.log(formattedDate);
+    //         console.log(selectedDate);
+    //         // 設置選中的日期
+    //         currentDate.setDate(selectedDate);
+    //         // 重新顯示日曆
+    //         showCalendar(currentDate.getFullYear(), currentDate.getMonth(), currentMonth, calendarDays);
+    //         // RootComponent.methods.setSelectedDate(currentDate);
+    //     }
+
+
+    // });
 
 
     // 操控價錢搜尋的值
@@ -106,9 +110,6 @@ $(document).ready(function () {
     }
 
 });
-
-
-
 
 
 // ------------------------------------------- vue框架 ----------------------------------------//
@@ -198,12 +199,34 @@ const RootComponent = {
 
 
         var searchInput = document.getElementById("search");
-        let mv = this;
+        // let mv = this;
         searchInput.addEventListener("input", function (event) {
-            mv.searchInput = event.target.value;
+            vm.searchInput = event.target.value;
             // mv.keywordSearch();
         });
 
+        // 選擇日期
+        document.querySelector('.calendar-days').addEventListener('click', (event) => {
+            // 判斷點擊的是否為日期
+            if (event.target.classList.contains('calendar-day')) {
+                // datep(event, currentDate);
+                // 獲取選中的日期
+                let selectedDate = event.target.innerText;
+                // 將選中的日期轉換為符合 OfferDate 格式的日期字串
+                const year = currentDate.getFullYear();
+                const month = currentDate.getMonth() + 1; // 月份從0開始，所以要加1
+                const formattedDate = `${year}-${month.toString().padStart(2, '0')}-${selectedDate.toString().padStart(2, '0')}`;
+
+                console.log(formattedDate);
+                console.log(selectedDate);
+                // 設置選中的日期
+                currentDate.setDate(selectedDate);
+                // 重新顯示日曆
+                showCalendar(currentDate.getFullYear(), currentDate.getMonth());
+                vm.selectedDate = currentDate;
+                vm.applyFilters();
+            }
+        });
     },
 
     computed: {
@@ -215,7 +238,6 @@ const RootComponent = {
     },
 
     methods: {
-
         // 幫價錢每三位數增加一個","
 
         numberWithCommas(value) {
@@ -236,6 +258,7 @@ const RootComponent = {
             // 價錢篩選
             this.displayItems = this.productItems.filter(item => {
                 const price = parseFloat(item.Price);
+                // console.log(item.OfferDate)
                 return price >= parseFloat(this.minPrice) && price <= parseFloat(this.maxPrice);
             });
 
@@ -262,17 +285,24 @@ const RootComponent = {
             }
 
             // Date filtering
+            console.log(this.selectedDate)
             if (this.selectedDate) {
                 this.displayItems = this.displayItems.filter(item => {
-                    // Convert selectedDate to the format used by the item's date
-                    const itemDate = new Date(item.Date);
-                    const selectedDate = new Date(this.selectedDate);
+                    // Find the corresponding productdetail item by ID
+                    const productDetailItem = this.productDetails.find(detail => {
+                        // Convert selectedDate to the format used by the item's date
+                        const itemDate = new Date(detail.OfferDate);
+                        const selectedDate = new Date(this.selectedDate);
 
-                    // Compare only the date portion (ignore time)
-                    itemDate.setHours(0, 0, 0, 0);
-                    selectedDate.setHours(0, 0, 0, 0);
+                        // Compare only the date portion (ignore time)
+                        itemDate.setHours(0, 0, 0, 0);
+                        selectedDate.setHours(0, 0, 0, 0);
 
-                    return itemDate.getTime() === selectedDate.getTime();
+                        return detail.ProductID === item.ID && itemDate.getTime() === selectedDate.getTime();
+                    });
+                    if (productDetailItem) return true;
+
+                    return false; // Item not found in productdetail, exclude it
                 });
             }
 
@@ -280,7 +310,7 @@ const RootComponent = {
             this.setPages();
         },
 
-
+        // findIndex()
         // -------------------------控制分頁-------------------
 
         setPages() {
---
+++ php/roomBlog.php
@@ -0,0 +1,29 @@
+<?php
+
+include('Mysql.php'); //資料庫連線
+
+// 第一個查詢 (所有product table的資料)
+$sql = "SELECT * FROM roomblog";
+
+$statement = $pdo->prepare($sql);
+$statement->execute();
+$data = $statement->fetchAll();
+
+var_dump($data);
+
+
+// 將查詢組成陣列
+
+$response = [
+    'roomblog' => $data,
+];
+
+// 轉 JSON
+$jsonData = json_encode($response);
+
+// 指定內容類型為 JSON
+header('Content-Type: application/json');
+
+echo $jsonData;
+
+?>
