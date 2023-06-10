f4919c6318e97ea6b70aef7b40d55e8dc06a6850
chieh0908 <xxaaxacc@gmail.com>
2023-06-10 13:53:59 +0800

    backblog刪除鍵 測試

--- php/deleteBlog.php
+++ php/deleteBlog.php
@@ -5,6 +5,7 @@ include('Mysql.php'); // 資料庫連線
 $checkedData = json_decode($_POST['data'], true);
 // print_r($checkedData);
 
+$successFlag = false; // 标志变量，用于跟踪是否至少有一次删除成功
 foreach ($checkedData as $ID){
     // echo $ID;
     $sqlDeleteBlogBlock = "DELETE FROM blogBlock WHERE BlogID = :ID";
@@ -20,13 +21,15 @@ foreach ($checkedData as $ID){
     $rowCount = $stmt->rowCount(); // 获取受影响的行数
 
     if ($rowCount > 0) {
+        $successFlag = true;
+    }
+}
+if ($successFlag > 0) {
         // 删除成功
         echo "done";
     } else {
         // 删除失败
         echo "Failed";
     }
-}
-
 
 ?>
